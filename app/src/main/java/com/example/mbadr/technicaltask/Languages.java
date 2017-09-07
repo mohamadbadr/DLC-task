4eb49256be053860dd9c31c93914db4795f367b8
mohamadbadr <m.badr94@gmail.com>
2017-09-07 04:36:55 +0200

    HttpHandler.java - languages activity are added

---
+++ app/src/main/java/com/example/mbadr/technicaltask/Languages.java
@@ -0,0 +1,21 @@
+package com.example.mbadr.technicaltask;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Languages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_languages);
+    }
+
+
+}
+
+
