54575a9af1dc0b101cf9aea1fd74f9f2e36ae516
mohamadbadr <m.badr94@gmail.com>
2017-09-08 03:56:34 +0200

    adding GetLanguages Class + its preExecute method and an empty doInBackground method

--- app/src/main/java/com/example/mbadr/technicaltask/HttpHandler.java
+++ app/src/main/java/com/example/mbadr/technicaltask/HttpHandler.java
@@ -1,3 +1,5 @@
+package com.example.mbadr.technicaltask;
+
 import android.util.Log;
 
 import java.io.BufferedInputStream;
@@ -5,10 +7,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpCookie;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 /**
--- app/src/main/java/com/example/mbadr/technicaltask/Languages.java
+++ app/src/main/java/com/example/mbadr/technicaltask/Languages.java
@@ -1,19 +1,41 @@
 package com.example.mbadr.technicaltask;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Languages extends AppCompatActivity {
 
+    private static String url = "https://www.memphistours.com/ws/languages/all_languages/ ";
+
+    private String TAG = Languages.class.getSimpleName();
+
+    private ProgressDialog pDialog;
+    private ListView lv;
+
+    ArrayList<HashMap<String,String>> languagesList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_languages);
+
+        languagesList =  new ArrayList<>();
+
+        lv = (ListView) findViewById(R.id.respons);
+//        new GetLanguages().execute();
     }
+
+
 
 
 }
